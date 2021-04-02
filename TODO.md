bc8457075b08330d10cf4e9f72b5f09e0856e63d
Kalp pawar <kalppawar10@gmail.com>
2021-04-02 20:43:41 +0530

    user tasks created

--- TODO.md
+++ TODO.md
@@ -1,6 +1,10 @@
 ## add todo and assigned user   ---([X] mark when done)
 
 * installation and file structure
+* attendance.yml 2 edits
+* faq 1 edit
 * intent
 * stories
 * actions
+
+* domain.yml 
--- actions/actions.py
+++ actions/actions.py
@@ -9,19 +9,58 @@
 
 # from typing import Any, Text, Dict, List
 #
-# from rasa_sdk import Action, Tracker
-# from rasa_sdk.executor import CollectingDispatcher
+from rasa_sdk import Action, Tracker
+from rasa_sdk.executor import CollectingDispatcher
 #
 #
-# class ActionHelloWorld(Action):
-#
-#     def name(self) -> Text:
-#         return "action_hello_world"
-#
-#     def run(self, dispatcher: CollectingDispatcher,
-#             tracker: Tracker,
-#             domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
-#
-#         dispatcher.utter_message(text="Hello World!")
-#
-#         return []
+
+class ActionAskUsn(Action):
+
+    def name(self) -> Text:
+        return "action_ask_usn"
+
+    def run(self, dispatcher: CollectingDispatcher,
+            tracker: Tracker,
+            domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
+        str((tracker.latest_message)['text'])
+        dispatcher.utter_message(template="utter_ask_usn")
+
+        return []
+
+
+class ActionAskPassword(Action):
+
+    def name(self) -> Text:
+        return "action_ask_password"
+
+    def run(self, dispatcher: CollectingDispatcher,
+            tracker: Tracker,
+            domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
+        str((tracker.latest_message)['text'])
+        dispatcher.utter_message(template="utter_ask_password")
+
+        return []
+
+class DisplayUpcomingHolidays(Action):
+
+    def name(self) -> Text:
+        return "display_upcoming_holidays"
+
+    def run(self, dispatcher: CollectingDispatcher,
+            tracker: Tracker,
+            domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
+
+        today = date.today()
+        print("Today's date:", today)
+        this_month = today.strftime("%m")
+        df2 = pd.read_excel('2021_calendar.xlsx')
+        df2['Date'] = pd.to_datetime(df2['Date'])
+        current_month_df = df2[df2['Date'].dt.month == int(this_month)]
+        content = 'Total of ' + str(current_month_df.shape[0]) + ' this month\n\n'
+        for i in range(current_month_df.shape[0]):
+            content = content + str(current_month_df['Date'].values[i])[:10] + '  -  ' + str(
+                current_month_df['Holiday Description'].values[i]) + '\n'
+
+        dispatcher.utter_message(text=content)
+
+        return []
